22be15a2ff38b197793e9a9302232e07a120c37d
Killian Stacey <killian@arbox.com>
2019-11-07 16:26:21 -0800

    Adding more views to polls as well as first template

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+# import django
+# from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question {}.".format(question_id))
+
+
+def results(request, question_id):
+    response = "You're looking at result of question {}".format(question_id)
+    return HttpResponse(response)
+
+
+def vote(request, question_id):
+    return HttpResponse("you're voting on question {}".format(question_id))
