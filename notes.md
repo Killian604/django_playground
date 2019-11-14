42ced847f4564914dc0fb1ae3ef5f4deb07f84fa
Killian Stacey <killian@arbox.com>
2019-11-13 16:18:40 -0800

    New tests added, views updated (notably Index), notes updated

--- notes.md
+++ notes.md
@@ -1,10 +1,22 @@
-https://docs.djangoproject.com/en/2.2/intro/tutorial04/
-
-
+https://docs.djangoproject.com/en/2.2/intro/tutorial05/
 
 
 ##### Order of migrations execution. Use it after changing your model(s) in models.py
-python manage.py ...->
+python manage.py ___->
 - makemigrations polls
 - sqlmigrate polls 0001
 - migrate
+
+---
+
+```
+from django.test.utils import setup_test_environment
+setup_test_environment()
+from django.test import Client
+client = Client()
+from django.urls import reverse
+response = client.get(reverse('polls:index'))
+response.status_code
+response.content
+response.context['latest_question_list']
+```
--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,10 @@
-from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse, HttpResponseRedirect
-from django.template import loader
-from django.urls import reverse
-from .models import Question, Choice
+from .models import Choice, Question
+
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
-
-from .models import Choice, Question
 
 
 class IndexView(generic.ListView):
@@ -16,8 +12,11 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
